d9ebe5419314e83f2948f5b13f2a83c320ccc41e
Therese22184 <157068534+Therese22184@users.noreply.github.com>
2024-01-25 16:09:29 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,73 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+#therese
+#emme
+#allison
+print("Hello, wold")
+Hello, wold
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+>>> print("Today", "is", "Wednesday")
+Today is Wednesday
+>>> print("Today" + "is" + "Wednesday")
+TodayisWednesday
+>>> #prediction: a b and c all on separate lines
+>>> print("a\nb\nc")
+a
+b
+c
+>>> #prediction2: result will be HelloWorld all together
+>>> print("Hello" + "World")
+HelloWorld
+>>> #prediction3: Hello, empty line, World!
+>>> print("Hello\n"+"\nWorld!")
+Hello
+
+World!
+>>> print("123\n45\n6")
+123
+45
+6
+>>> 1+2*3
+7
+>>> (1+2)*3
+9
+>>> 7/3
+2.3333333333333335
+>>> 7//3
+2
+>>> 7%3
+1
+>>> 22/4
+5.5
+>>> 22//4
+5
+>>> 22%4
+2
+>>> 3**2
+9
+>>> 2**3
+8
+>>> #python follows the usual order of operations
+>>> #a single / divides the first number by the second, while // divides the 1st number by the 2nd and makes the answer an interger
+>>> #the % sign gives the literall remainder of the 1st number divided by the 2nd
+>>> # ** puts the 1st number to the power of the 2nd
+>>> #prediction: (4+3)//2 would equal 3
+>>> #prediction: 6%2 would equal 0
+>>> (4+3)//2
+3
+>>> 6%2
+0
+>>> 1+2*3**4
+163
+>>> print("2+2=", 2+2)
+2+2= 4
+>>> print("50 pounds is", 50*0.453592, "kilograms")
+50 pounds is 22.6796 kilograms
